091d7fd8d753f0d6a227581750ca7a8c26ec1acd
spell-26 <jsc.2612@gmail.com>
2024-02-05 11:55:58 -0500

    deploy attemp 1

--- src/app/modules/administrador/utils/sharedMethods/env/env.service.ts
+++ src/app/modules/administrador/utils/sharedMethods/env/env.service.ts
@@ -8,9 +8,7 @@ export class EnvService {
   constructor() { }
 
   port : number = 8080;
-  //Local URL
-  /*url : string = `http://localhost:${this.port}/api/v1`;*/
-  //production url
+
   url : string = `https://la-burgueseria.onrender.com/api/v1`
   getPort() : number {
     return this.port;
